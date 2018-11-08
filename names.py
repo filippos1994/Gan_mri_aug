96031dc1f24b64e467804c30c51e776eea5a30ef
filippos1994 <phkonidaris@gmail.com>
2018-11-08 19:38:00 +0200

    changed numbering scheme on 1_preprocessing, added subjects list on separate directory and corresponding script

---
+++ names.py
@@ -0,0 +1,55 @@
+from os import listdir
+
+type_dict = {'ad', 'nor'}
+set_dict = {'train', 'test', 'valid'}
+
+for dtype in type_dict:
+	for dset in set_dict:
+
+		ttv_split_dir = "/home/filippos/diplomatiki/adni/nifti/all/ttv_split_{}/{}/{}/".format(dtype, dset, dtype)
+		name_list = listdir(ttv_split_dir)
+		print(name_list)
+		
+		subjects_path = 'subjects_{}_{}.txt'.format(dset, dtype)
+		try:
+			subjects_file = open(subjects_path, 'w')
+		except IOError:
+			# If not exists, create the file
+			subjects_file = open(subjects_path, 'w+')
+
+		for name in name_list:
+			subjects_file.write(name + "\n")
+
+		subjects_file.close()
+
+
+
+
+#ttv_split_dir = "/home/filippos/diplomatiki/adni/nifti/all/ttv_split_{}/{}/".format(dtype, dset)
+
+"""name_list = []
+for _, _, files in walk(ttv_split_dir):
+	for file in files:
+		name_list.append(file.split('/')[0])
+
+name_list = list(set(name_list))
+print(name_list)
+"""
+
+#name_list = listdir(ttv_split_dir)
+#print(name_list)
+
+
+
+
+#visits_list = ["s1/e1", "s1/e2", "s2/e1", "s2/e2", "s2/e3"]
+#name_list = []
+
+"""for visit in visits_list:
+	name_list.append(visit.split('/')[0])
+
+print(name_list)
+
+name_list = list(set(name_list))
+print(name_list)
+"""

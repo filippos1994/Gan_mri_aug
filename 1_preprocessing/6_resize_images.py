644f4dcffe95f3a646084f8c80e0871de75e497a
filippos1994 <phkonidaris@gmail.com>
2018-11-12 20:08:17 +0200

    added image resizing script

---
+++ 1_preprocessing/6_resize_images.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+from os import path, walk
+from PIL import Image
+
+for root, _, files in walk("data"):
+	for file in files:
+		img = Image.open(path.join(root, file))
+		img = img.resize((160,192), resample = Image.LANCZOS)
+
+		dst = root.replace("data", "data_resized")
+		img.save(path.join(dst, file))
